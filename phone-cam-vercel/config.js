0c075cf852d45cac1e64e80ddf5a22868b842627
VUTH VIREAK <vothvirak@gmail.com>
2026-06-12 04:14:16 +0700

    Add files via upload

---
+++ phone-cam-vercel/config.js
@@ -0,0 +1,9 @@
+// After you deploy the backend on Render, replace this URL with your Render URL.
+// Example:
+// window.APP_CONFIG = {
+//     SIGNAL_SERVER: "https://phone-cam-server-xxxx.onrender.com"
+// };
+
+window.APP_CONFIG = {
+    SIGNAL_SERVER: "https://phone-cam-server.onrender.com/"
+};
